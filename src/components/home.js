fc28999f7f8196cd5d48cc545ff1134fbfaefb46
Harry Muir <harry182894@gmail.com>
2020-09-05 14:14:12 -0600

    Modified media query to fix responsive

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css';
 
 const Home = ({ name, paragraph, authorImage }) => (
   <div className={styles.homeContainer}>
-    <Row>
+    <Row className={styles.content}>
       <Col md={8}>
         <div className={styles.homeHeadWrapper}>
           <h1> Hello I 'm <br></br> <span>{name}</span></h1>
