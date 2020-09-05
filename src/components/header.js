4c8526de2633d14e00f0bc0bb9a730fd95621889
Harry Muir <harry182894@gmail.com>
2020-09-05 14:31:06 -0600

    Adjusted Navbar and added new item to the menu

--- src/components/header.js
+++ src/components/header.js
@@ -4,15 +4,20 @@ import { Link } from 'react-router-dom';
 import styles from '../styles/Header.module.css';
 
 const Header = ({ contactEmail }) => (
-  <div>
-    <div className={styles.headerContainer}>
-      <nav>
+  <div className={styles.headerContainer}>
+    <nav>
+      <div>
+        <ul>
+          <li><Link to="/">Harry Muir</Link></li>
+        </ul>
+      </div>
+      <div className={styles.rightSide}>
         <ul>
           <li><Link to="/about">Resume</Link></li>
           <li><a href={`mailto:${contactEmail}`}>Contact</a></li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
   </div>
 );
 
