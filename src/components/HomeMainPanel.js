366fce7518da576ab2e3c4baff324ab0f011b16e
Harry Muir <harry182894@gmail.com>
2020-09-12 01:35:59 -0600

    Added media queries and fixed responsive for Home view

--- src/components/HomeMainPanel.js
+++ src/components/HomeMainPanel.js
@@ -6,13 +6,13 @@ import styles from 'styles/Home.module.css';
 const Home = ({ name, paragraph, authorImage }) => (
   <div className={styles.homeContainer}>
     <Row className={styles.content}>
-      <Col md={8}>
+      <Col className={styles.textCol} md={8}>
         <div className={styles.homeHeadWrapper}>
           <h1> Hey I 'm <br></br> <span>{name}</span></h1>
           <p>{paragraph}</p>
         </div>
       </Col>
-      <Col md={4}>
+      <Col className={styles.imgCol} md={4}>
         <div className={styles.imageContainer}>
           <img src={authorImage} alt="author" />
           <div className="bg" />
